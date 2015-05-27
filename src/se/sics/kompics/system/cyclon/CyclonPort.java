14d33fcfcbbe76d6482cc06fe9d944dc6ff8c0a2
Amir H. Payberah <amir@sics.se>
2015-05-27 15:50:21 +0200

    delete

--- src/se/sics/kompics/system/cyclon/CyclonPort.java
+++
@@ -1,8 +0,0 @@
-package se.sics.kompics.system.cyclon;
-
-import se.sics.kompics.PortType;
-
-public final class CyclonPort extends PortType {{
-	negative(Join.class);
-	positive(JoinCompleted.class);
-}}
--- src/se/sics/kompics/system/peer/PeerPort.java
+++
@@ -1,7 +0,0 @@
-package se.sics.kompics.system.peer;
-
-import se.sics.kompics.PortType;
-
-public class PeerPort extends PortType {{
-	negative(JoinPeer.class);
-}}
